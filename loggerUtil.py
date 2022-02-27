0990b48f4c3e869ead3f2c463761420573dd2b1f
Manav Mehta <mmanavmehta@gmail.com>
2022-02-27 21:10:40 +0530

    Logging: move configurations to logger.config

--- loggerUtil.py
+++ loggerUtil.py
@@ -1,8 +1,29 @@
+import configparser
 import logging
 
+config = configparser.ConfigParser()
+config.read('logger.config')
+
+def getLogLevel(configLogLevel):
+    if(configLogLevel == 'DEBUG'):
+        return logging.DEBUG
+    
+    elif(configLogLevel == 'INFO'):
+        return logging.INFO
+    
+    elif(configLogLevel == 'WARNING'):
+        return logging.WARNING
+    
+    elif(configLogLevel == 'ERROR'):
+        return logging.ERROR
+    
+    else:
+        return logging.CRITICAL
+
 def getLogger():
-    logging.basicConfig(filename="Logs/0.log",
+    logging.basicConfig(filename=config['DEFAULT']['Filename'],
                     format='[%(asctime)s] [%(levelname)s] [%(message)s]',
-                    filemode='a',
-                    level=logging.DEBUG)
+                    filemode=config['DEFAULT']['Filemode'],
+                    level=getLogLevel(config['DEFAULT']['LogLevel']))
+    
     return logging.getLogger()
